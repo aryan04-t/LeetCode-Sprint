f28467c5b13158c52fcc20413f9bd722ff6cfe75
Aryan Tomar <aryantomar5000@gmail.com>
2025-11-23 22:33:29 +0530

    3754. Concatenate Non-Zero Digits and Multiply by Sum I
    --> Most Optimal

---
+++ 3754. Concatenate Non-Zero Digits and Multiply by Sum I/01_Most_Optimal.cpp
@@ -0,0 +1,37 @@
+// LeetCode (3754. Concatenate Non-Zero Digits and Multiply by Sum I): 
+// https://leetcode.com/problems/concatenate-non-zero-digits-and-multiply-by-sum-i 
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+using ll = long long;
+
+
+class Solution {
+public:
+    long long sumAndMultiply(int n) {
+        
+        ll x = 0;
+        ll sum = 0;
+        ll tens = 1;
+
+        while (n) {
+            ll digit = n % 10;
+            if (digit) {
+                x = digit * tens + x;
+                tens *= 10;
+            }
+            n /= 10;
+
+            sum += digit;
+        }
+
+        return (x * sum);
+    }
+};
+
+
+// T.C. = O(log(n) to the base 10) 
+// S.C. = O(1) 
