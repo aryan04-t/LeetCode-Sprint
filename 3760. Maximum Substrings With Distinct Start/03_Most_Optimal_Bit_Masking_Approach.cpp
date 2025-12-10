d111d28263bbb41c411062a0d853724c78396aef
Aryan Tomar <aryantomar5000@gmail.com>
2025-12-10 12:13:50 +0530

    3760. Maximum Substrings With Distinct Start
    --> Most Optimal Bit Masking Approach

---
+++ 3760. Maximum Substrings With Distinct Start/03_Most_Optimal_Bit_Masking_Approach.cpp
@@ -0,0 +1,38 @@
+// LeetCode (3760. Maximum Substrings With Distinct Start): 
+// https://leetcode.com/problems/maximum-substrings-with-distinct-start 
+
+
+// TAGS: [RATING: 1364], [MEDIUM], [NAIVE OPTIMAL], [STRING], [BIT MASKING], [BIT MANIPULATION] 
+
+
+#include<bits/stdc++.h>
+using namespace std; 
+
+
+class Solution {
+public:
+    int maxDistinct(string s) {
+        
+        int seen = 0;
+        int ans = 0;
+
+        for (char ch : s) {
+            int shifts = ch - 'a';
+            int bit = 1 << shifts;     // computing bit position for char
+            
+            if ((seen & bit) == 0) {   // checking if that bit is not set 
+                seen |= bit;           // set the bit 
+                ans++;                 // count the unique char occurence 
+                if (ans == 26) break;  // maximum achievable ans 
+            } 
+        }
+
+        return ans;
+    }
+};
+
+
+// T.C. = O(n) 
+// S.C. = O(1) 
+
+// n = s.length() 
