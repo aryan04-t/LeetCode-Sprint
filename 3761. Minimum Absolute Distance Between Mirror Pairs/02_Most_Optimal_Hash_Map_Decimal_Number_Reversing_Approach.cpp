5567817a222f27aa9763643fb0248dd0c123e48e
Aryan Tomar <aryantomar5000@gmail.com>
2025-12-10 15:17:06 +0530

    3761. Minimum Absolute Distance Between Mirror Pairs
    --> Most Optimal, Hash Map and Decimal Number Reversing Approach

---
+++ 3761. Minimum Absolute Distance Between Mirror Pairs/02_Most_Optimal_Hash_Map_Decimal_Number_Reversing_Approach.cpp
@@ -0,0 +1,57 @@
+// LeetCode (3761. Minimum Absolute Distance Between Mirror Pairs): 
+// https://leetcode.com/problems/minimum-absolute-distance-between-mirror-pairs 
+
+
+// TAGS: [RATING: 1668], [MEDIUM], [MOST OPTIMAL], [HASH MAP], [REVERSING], [DECIMAL NUMBER] 
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int reverseNumber (int num) {
+        
+        int revNum = 0;
+
+        while (num) {
+            int digit = num % 10;
+            revNum = revNum * 10 + digit;
+            num /= 10;
+        }
+
+        return revNum;
+    }
+
+    int minMirrorPairDistance(vector<int>& nums) {
+
+        int n = nums.size();
+        int mini = INT_MAX;
+        unordered_map<int, int> numIndexMap;
+        
+        for (int i=n-1; i >= 0; i--) {
+
+            int num = nums[i];
+            int revNum = reverseNumber(num);
+            
+            if (numIndexMap.count(revNum)) {
+                int j = numIndexMap[revNum];
+                int absDist = abs(i - j);
+                mini = min(mini, absDist);
+            }
+        
+            numIndexMap[num] = i;
+        }
+
+        if (mini == INT_MAX) mini = -1;
+        
+        return mini;
+    }
+};
+
+
+// T.C. = O(n*(log(max_num) to the base 10)) 
+// S.C. = O(n) 
+
+// n = nums.size(), max_num = the maximum value in the vector nums
