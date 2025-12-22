83439fd744b855fb2a91b8236576297de61ada35
Aryan Tomar <aryantomar5000@gmail.com>
2025-12-22 12:53:30 +0530

    3779. Minimum Number of Operations to Have Distinct Elements
    --> Optimal

---
+++ 3779. Minimum Number of Operations to Have Distinct Elements/01_Optimal.cpp
@@ -0,0 +1,56 @@
+// LeetCode (3779. Minimum Number of Operations to Have Distinct Elements): 
+// https://leetcode.com/problems/minimum-number-of-operations-to-have-distinct-elements 
+
+
+// TAGS: [RATING: N/A], [MEDIUM], [OPTIMAL], [ARRAY], [SIMULATION], [HASH MAP], [COUNTING] 
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        
+        int n = nums.size();
+
+        unordered_map<int, int> freqCount;
+        int duplicates = 0;
+
+        for (int i=0; i < n; i++) {
+            int val = nums[i];
+            if (freqCount[val] == 1) {
+                duplicates++;
+            }
+            freqCount[val]++;
+        }
+
+        int ops = 0;
+        for (int i=0; i < n; i+=3) {
+
+            if (duplicates == 0) break;
+            int j = i;
+
+            while (j < n && j < i+3) {
+                int val = nums[j];
+                freqCount[val]--;
+
+                if (freqCount[val] == 1) {
+                    duplicates--;
+                }
+                j++;
+            }
+
+            ops++;
+        }
+
+        return ops;
+    }
+};
+
+
+// T.C. = O(n) 
+// S.C. = O(m) 
+
+// m = total number of unique elements in nums 
