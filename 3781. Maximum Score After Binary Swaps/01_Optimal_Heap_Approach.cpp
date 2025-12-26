cef4887f8974b2c47f6a9b70bcc68660cc69d8b6
Aryan Tomar <aryantomar5000@gmail.com>
2025-12-27 00:54:53 +0530

    3781. Maximum Score After Binary Swaps
    --> Optimal Heap Approach

---
+++ 3781. Maximum Score After Binary Swaps/01_Optimal_Heap_Approach.cpp
@@ -0,0 +1,35 @@
+// LeetCode (3781. Maximum Score After Binary Swaps): 
+// https://leetcode.com/problems/maximum-score-after-binary-swaps 
+
+
+// TAGS: [RATING: N/A], [MEDIUM], [OPTIMAL], [ARRAY], [STRING], [GREEDY], [MAXIMIZING], [SUM], [HEAP], [MAX HEAP] 
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    long long maximumScore(vector<int>& nums, string s) {
+        
+        int n = s.length();
+        
+        priority_queue<int> maxHeap;
+        long long maxiSum = 0;
+
+        for (int i=0; i < n; i++) {
+            maxHeap.push(nums[i]);
+            if (s[i] == '1') {
+                maxiSum += maxHeap.top();
+                maxHeap.pop();
+            }
+        }
+
+        return maxiSum;
+    }
+};
+
+
+// T.C. = O(n*log(n)) 
+// S.C. = O(n) 
