37356a3603c6708d2f5e20fd5dbb3d952e5d8765
Aryan Tomar <aryantomar5000@gmail.com>
2026-01-02 23:48:11 +0530

    961. N-Repeated Element in Size 2N Array
    --> Optimal Hash Set Approach

---
+++ 961. N-Repeated Element in Size 2N Array/00_Optimal_Hash_Set_Approach.cpp
@@ -0,0 +1,29 @@
+// LeetCode (961. N-Repeated Element in Size 2N Array): 
+// https://leetcode.com/problems/n-repeated-element-in-size-2n-array 
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        
+        int m = nums.size();
+        int n = m / 2;
+
+        unordered_set<int> st;
+        for (int i=0; i <= n+1; i++) {
+            int num = nums[i];
+            if (st.count(num)) return num;
+            st.insert(num);
+        }
+
+        return -1;
+    }
+};
+
+
+// T.C. = O(n+2) = O(n) 
+// S.C. = O(n+1) = O(n) 
