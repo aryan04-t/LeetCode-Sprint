2779302703d099bc9d2eb4beccbcdfb7fe1e5118
Aryan Tomar <aryantomar5000@gmail.com>
2026-01-08 23:58:53 +0530

    66. Plus One
    --> Naive Approach

---
+++ 66. Plus One/00_Naive_Approach.cpp
@@ -0,0 +1,36 @@
+// LeetCode (66. Plus One): 
+// https://leetcode.com/problems/plus-one 
+
+
+// TAGS: [RATING: N/A], [EASY], [NAIVE], [STL], [ARRAY], [DECIMAL NUMBER], [MATH], [SUM] 
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        
+        int n = digits.size();
+        reverse(digits.begin(), digits.end());
+
+        int carry = 0;
+        for (int i=0; i < n; i++) {
+            int incrementedDigit = digits[i] + carry + ((i == 0) ? 1 : 0);
+            int newDigit = incrementedDigit % 10;
+            digits[i] = newDigit;
+            carry = incrementedDigit / 10;
+        }
+
+        if (carry) digits.push_back(carry);
+        reverse(digits.begin(), digits.end());
+
+        return digits;
+    }
+};
+
+
+// T.C. = O(n/2) + O(n) + O(n/2) = O(2n) = O(n) 
+// S.C. = O(1) 
