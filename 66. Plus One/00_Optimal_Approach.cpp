df3a82b949915a1e07139ff13dcd675ccfd7c716
Aryan Tomar <aryantomar5000@gmail.com>
2026-01-09 19:43:01 +0530

    66. Plus One
    --> Updated Tags

--- 66. Plus One/00_Optimal_Approach.cpp
+++ 66. Plus One/00_Optimal_Approach.cpp
@@ -2,7 +2,7 @@
 // https://leetcode.com/problems/plus-one 
 
 
-// TAGS: [RATING: N/A], [EASY], [OPTIMAL], [STL], [ARRAY], [DECIMAL NUMBER], [MATH], [SUM] 
+// TAGS: [RATING: N/A], [EASY], [OPTIMAL], [STL], [ARRAY], [DECIMAL NUMBER], [MATH], [SUM], [REVERSING] 
 
 
 #include<bits/stdc++.h>
