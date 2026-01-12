6e0f270f7e383e2ea5bf24b3fa6648f6d3e34212
Aryan Tomar <aryantomar5000@gmail.com>
2026-01-12 22:01:11 +0530

    3803. Count Residue Prefixes
    --> Most Optimal Bit Manipulation Approach

---
+++ 3803. Count Residue Prefixes/02_Most_Optimal_Bit_Manipulation_Approach.cpp
@@ -0,0 +1,44 @@
+// LeetCode (3803. Count Residue Prefixes): 
+// https://leetcode.com/problems/count-residue-prefixes 
+
+
+// TAGS: [RATING: N/A], [EASY], [MOST OPTIMAL], [STRING], [BIT MANIPULATION], [MODULAR ARITHMETIC] 
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int residuePrefixes(string s) {
+
+        int n = s.length();
+
+        int charSeenMask = 0;
+        int distinctChars = 0;
+        int ans = 0;
+        
+        for (int i=0; i < n; i++) {
+            char ch = s[i];
+            
+            int shifts = ch - 'a';
+            int charPos = 1 << shifts;
+            
+            bool isCharNotSeenBefore = !(charSeenMask & charPos); 
+            if (isCharNotSeenBefore) {
+                distinctChars++;
+                charSeenMask |= charPos;
+            }
+            
+            int prefixLen = i + 1;
+            if (distinctChars == (prefixLen % 3)) ans++;
+        }
+
+        return ans;
+    }
+};
+
+
+// T.C. = O(n) 
+// S.C. = O(1) 
