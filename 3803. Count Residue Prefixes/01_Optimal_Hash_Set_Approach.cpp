eea8f1f146dede066063ca5f381c8ac6220b0167
Aryan Tomar <aryantomar5000@gmail.com>
2026-01-12 21:31:23 +0530

    3803. Count Residue Prefixes
    --> Optimal Hash Set Approach

---
+++ 3803. Count Residue Prefixes/01_Optimal_Hash_Set_Approach.cpp
@@ -0,0 +1,37 @@
+// LeetCode (3803. Count Residue Prefixes): 
+// https://leetcode.com/problems/count-residue-prefixes 
+
+
+// TAGS: [RATING: N/A], [EASY], [OPTIMAL], [STRING], [HASH SET], [MODULAR ARITHMETIC] 
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int residuePrefixes(string s) {
+
+        int n = s.length();
+    
+        unordered_set<char> uniqueChars;
+        int ans = 0;
+        
+        for (int i=0; i < n; i++) {
+            char ch = s[i];
+            uniqueChars.insert(ch);
+
+            int totalUniqueChars = static_cast<int>(uniqueChars.size());
+            int prefixLen = i + 1;
+            
+            if (totalUniqueChars == (prefixLen % 3)) ans++;
+        }
+
+        return ans;
+    }
+};
+
+
+// T.C. = O(n) 
+// S.C. = O(26) = O(1)  
