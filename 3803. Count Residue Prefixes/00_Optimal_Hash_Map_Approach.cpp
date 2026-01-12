02d9e42c2c16809b3c0ff519ffe446a196793c10
Aryan Tomar <aryantomar5000@gmail.com>
2026-01-12 21:27:13 +0530

    3803. Count Residue Prefixes
    --> Optimal Hash Map Approach

---
+++ 3803. Count Residue Prefixes/00_Optimal_Hash_Map_Approach.cpp
@@ -0,0 +1,38 @@
+// LeetCode (3803. Count Residue Prefixes): 
+// https://leetcode.com/problems/count-residue-prefixes 
+
+
+// TAGS: [RATING: N/A], [EASY], [OPTIMAL], [STRING], [HASH MAP], [MODULAR ARITHMETIC] 
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int residuePrefixes(string s) {
+
+        int n = s.length();
+    
+        unordered_map<int, int> charFreq;
+        int distinctChars = 0;
+        int ans = 0;
+        
+        for (int i=0; i < n; i++) {
+            char ch = s[i];
+            
+            charFreq[ch]++;
+            if (charFreq[ch] == 1) distinctChars++;
+
+            int prefixLen = i + 1;
+            if (distinctChars == (prefixLen % 3)) ans++;
+        }
+
+        return ans;
+    }
+};
+
+
+// T.C. = O(n) 
+// S.C. = O(n) 
