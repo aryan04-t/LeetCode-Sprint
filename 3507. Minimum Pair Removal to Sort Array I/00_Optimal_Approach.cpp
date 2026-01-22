80e6af04520bd0aec1ff5661eb215e7be4d4eaa6
Aryan Tomar <aryantomar5000@gmail.com>
2026-01-22 23:37:00 +0530

    3507. Minimum Pair Removal to Sort Array I
    --> Optimal Simulation Approach

---
+++ 3507. Minimum Pair Removal to Sort Array I/00_Optimal_Approach.cpp
@@ -0,0 +1,78 @@
+// LeetCode (3507. Minimum Pair Removal to Sort Array I): 
+// https://leetcode.com/problems/minimum-pair-removal-to-sort-array-i 
+
+
+// TAGS: [RATING: 1348], [EASY], [OPTIMAL], [ARRAY], [SIMULATION], [SUM], [MINIMIZING], [MAKE NON-DECREASING SORTED] 
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int findMinAdjacentPairStartIndex(const vector<int> &nums) {
+        
+        int i = nums.size() - 1;
+        int minAdjPairStartIdx = -1;
+
+        int minSum = INT_MAX;
+
+        while (i >= 1) {
+            int sum = nums[i-1] + nums[i];
+            if (sum <= minSum) {
+                minSum = sum;
+                minAdjPairStartIdx = i-1;
+            }
+            i--;
+        }
+
+        return minAdjPairStartIdx;
+    }
+
+    bool isArrayNonDecreasing (const vector<int>& nums) {
+
+        int n = nums.size();
+        int i = 1;
+
+        while (i < n) {
+            if (nums[i-1] > nums[i]) return false;
+            i++;
+        }
+
+        return true;
+    }
+
+    int minimumPairRemoval(vector<int>& nums) {
+        
+        vector<int> newNums;
+        int minMoves = 0;
+
+        while (!isArrayNonDecreasing(nums)) {
+
+            int minAdjPairStartIdx = findMinAdjacentPairStartIndex(nums); 
+            
+            int n = nums.size();
+            int i=0; 
+
+            while (i < n) {
+                if (minAdjPairStartIdx == i) {
+                    newNums.push_back(nums[i] + nums[i+1]);
+                    i += 2;
+                    continue;
+                }
+                newNums.push_back(nums[i]);
+                i++;
+            }
+
+            nums = move(newNums);
+            minMoves++;
+        }
+        
+        return minMoves;
+    }
+};
+
+
+// T.C. = O(n^2) 
+// S.C. = O(n) 
