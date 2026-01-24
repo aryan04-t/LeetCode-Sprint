9234cac60327d4e17e9f8e988eb2e7cba126ddb7
Aryan Tomar <aryantomar5000@gmail.com>
2026-01-24 21:19:19 +0530

    3507. Minimum Pair Removal to Sort Array I
    --> Naive Ordered Set Approach

---
+++ 3507. Minimum Pair Removal to Sort Array I/03_Naive_Ordered_Set_Approach.cpp
@@ -0,0 +1,49 @@
+// LeetCode (3507. Minimum Pair Removal to Sort Array I): 
+// https://leetcode.com/problems/minimum-pair-removal-to-sort-array-i 
+
+
+// TAGS: [RATING: 1348], [EASY], [NAIVE], [ARRAY], [SIMULATION], [SUM], [MINIMIZING], [ORDERED SET], [CHECK IS ARRAY SORTED], [MAKE ARRAY NON-DECREASING SORTED] 
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int findMinAdjacentPairStartIndex(const vector<int> &nums) {
+
+        int n = nums.size();
+        set<pair<int, int>> orderedSet;
+
+        for (int i = 0; i < n-1; i++) {
+            int sum = nums[i] + nums[i+1];
+            orderedSet.insert({ sum, i });
+        }
+
+        set<pair<int, int>>::iterator it = orderedSet.begin();
+
+        return it->second;
+    }
+
+    int minimumPairRemoval(vector<int>& nums) {
+        
+        int minMoves = 0;
+
+        while (!is_sorted(nums.begin(), nums.end())) {
+
+            int minAdjPairStartIdx = findMinAdjacentPairStartIndex(nums); 
+
+            nums[minAdjPairStartIdx] += nums[minAdjPairStartIdx + 1];
+            nums.erase(nums.begin() + minAdjPairStartIdx + 1);
+
+            minMoves++;
+        }
+        
+        return minMoves;
+    }
+};
+
+
+// T.C. = O(n*((n*log(n)) + n)) = O((n^2)*log(n)) 
+// S.C. = O(n) 
