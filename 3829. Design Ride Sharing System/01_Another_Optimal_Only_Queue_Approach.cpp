3cbeb8b076e0089ff7a18903d10d401e96dc6228
Aryan Tomar <aryantomar5000@gmail.com>
2026-02-06 19:03:39 +0530

    3829. Design Ride Sharing System
    --> Optimal Only Queue Approach

---
+++ 3829. Design Ride Sharing System/01_Another_Optimal_Only_Queue_Approach.cpp
@@ -0,0 +1,75 @@
+// LeetCode (3829. Design Ride Sharing System): 
+// https://leetcode.com/problems/design-ride-sharing-system 
+
+
+// TAGS: [RATING: 1593], [MEDIUM], [OPTIMAL], [STL], [DESIGN], [QUEUE] 
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class RideSharingSystem {
+private:
+    queue<int> riderQ;  // S.C. = O(n) 
+    queue<int> driverQ; // S.C. = O(m) 
+    
+public:
+    RideSharingSystem() {
+        
+    }
+    
+    // T.C. = O(1); S.C. = O(1) 
+    void addRider(int riderId) {
+        riderQ.push(riderId);
+    }
+    
+    // T.C. = O(1); S.C. = O(1) 
+    void addDriver(int driverId) {
+        driverQ.push(driverId);
+    }
+    
+    // T.C. = O(1); S.C. = O(1) 
+    vector<int> matchDriverWithRider() {
+        
+        if (!riderQ.empty() && !driverQ.empty()) {
+            int riderId = riderQ.front();
+            int driverId = driverQ.front();
+
+            riderQ.pop();
+            driverQ.pop();
+            
+            return { driverId, riderId };
+        }
+
+        return { -1, -1 };
+    }
+    
+    // T.C. = O(n); S.C. = O(1) 
+    void cancelRider(int riderId) {
+        
+        queue<int> updatedRiderQ;
+
+        while (!riderQ.empty()) {
+            if (riderQ.front() != riderId) {
+                updatedRiderQ.push(riderQ.front());
+            }
+            riderQ.pop();
+        }
+
+        riderQ = move(updatedRiderQ);
+    }
+};
+
+
+// Here, n = total number of riders, m = total number of drivers 
+
+
+/*
+    Your RideSharingSystem object will be instantiated and called as such:
+    RideSharingSystem* obj = new RideSharingSystem();
+    obj->addRider(riderId);
+    obj->addDriver(driverId);
+    vector<int> param_3 = obj->matchDriverWithRider();
+    obj->cancelRider(riderId);
+*/
