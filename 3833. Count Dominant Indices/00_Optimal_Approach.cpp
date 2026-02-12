35eac9e7606379117c90fd8f71a069c4766b9b91
Aryan Tomar <aryantomar5000@gmail.com>
2026-02-12 19:14:04 +0530

    3833. Count Dominant Indices
    --> Updated Code and Added Tags

--- 3833. Count Dominant Indices/00_Optimal_Approach.cpp
+++ 3833. Count Dominant Indices/00_Optimal_Approach.cpp
@@ -2,7 +2,7 @@
 // https://leetcode.com/problems/count-dominant-indices 
 
 
-// TAGS: [RATING: N/A]
+// TAGS: [RATING: N/A], [EASY], [OPTIMAL], [ARRAY], [SUFFIX SUM], [AVERAGE], [COUNTING] 
 
 
 #include<bits/stdc++.h>
@@ -15,18 +15,16 @@ public:
 
         int n = nums.size();
 
-        double sum = nums[n-1];
-        double numbers = 1;
-
+        int sum = nums[n-1];
         int dominants = 0;
         
         for (int i = n-2; i >= 0; i--) {
-            double avg = sum / numbers;
-            if (static_cast<double>(nums[i]) > avg) {
+            int numbers = n-i-1;
+            int avg = sum / numbers;
+            if (nums[i] > avg) {
                 dominants++;
             }
             sum += nums[i];
-            numbers++;
         }
 
         return dominants;
