b422ca864ea516253c870cb7149665d675b13e4d
Aryan Tomar <aryantomar5000@gmail.com>
2026-02-17 06:38:34 +0530

    3842. Toggle Light Bulbs
    --> Another Optimal Approach

---
+++ 3842. Toggle Light Bulbs/01_Another_Optimal_Approach.cpp
@@ -0,0 +1,36 @@
+// LeetCode (3842. Toggle Light Bulbs): 
+// https://leetcode.com/problems/toggle-light-bulbs 
+
+
+// TAGS: [RATING: N/A], [EASY], [OPTIMAL], [ARRAY], [SIMULATION], [MODULAR ARITHMETIC] 
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> toggleLightBulbs(vector<int>& bulbs) {
+
+        vector<int> coutBulbClicks(101, 0);
+
+        for (int val : bulbs) {
+            coutBulbClicks[val]++;
+        }
+
+        vector<int> ans;
+        for (int i=1; i <= 100; i++) {
+            bool isBulbOn = coutBulbClicks[i] % 2;
+            if (isBulbOn) ans.push_back(i);
+        }
+
+        return ans;
+    }
+};
+
+
+// T.C. = O(n) 
+// S.C. = O(n) 
+
+// Here, n = 100 at max
